c61b7197bb4e1ac2092dbde42a47d785698ae85b
Christof Thalmann <christof.thalmann@lingvano.com>
2023-03-23 14:00:05 +0100

    add app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,22 @@
+import { ExpoConfig } from "@expo/config";
+
+const config: ExpoConfig = {
+  name: "Tet Blog",
+  slug: "test-blog",
+  scheme: "test-blog",
+  owner: "test-blog",
+  version: "0.1",
+  extra: {
+    eas: {
+      projectId: "42778430-dcc8-4c5f-b31c-75a53b3a1bf9",
+    },
+  },
+  android: {
+    package: "com.testBlog.app",
+  },
+  ios: {
+    bundleIdentifier: "com.testBlog.app",
+  },
+};
+
+export default (): ExpoConfig => config;
